265acca7d7fd9f11a82fa984f65cd7e062320d39
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2025-02-20 16:40:02 +0300

    Изменения в генерации Jwt-токенов. Использование RSA ключей. Добавлен фильтр Jwt токенов

--- user-service/src/main/java/io/github/zapolyarnydev/config/SecurityConfig.java
+++ user-service/src/main/java/io/github/zapolyarnydev/config/SecurityConfig.java
@@ -1,25 +1,61 @@
 package io.github.zapolyarnydev.config;
 
+import io.github.zapolyarnydev.jwt.JwtAuthenticationFilter;
+import io.github.zapolyarnydev.service.impl.UserServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
 
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/login", "/register").permitAll()
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/login", "/auth/register").permitAll()
                         .anyRequest().authenticated())
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider())
                 .httpBasic(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService());
+        return provider;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new UserServiceImpl();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter(){
+        return new JwtAuthenticationFilter(userDetailsService());
     }
 }
--- user-service/src/main/java/io/github/zapolyarnydev/jwt/JwtUtil.java
+++ user-service/src/main/java/io/github/zapolyarnydev/jwt/JwtUtil.java
@@ -1,45 +1,57 @@
 package io.github.zapolyarnydev.jwt;
 
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.util.Date;
 
 public class JwtUtil {
 
-    private static final String key = "TestZapolyarnyKeySecretSecret19DD232ERQ1232";
-    private static long expirationTime = 60 * 60 * 10 * 500;
+    private static final KeyPair keyPair = generateKeyPair();
+    private static final PrivateKey privateKey = keyPair.getPrivate();
+    private static final PublicKey publicKey = keyPair.getPublic();
+    private static final long EXPIRATION_TIME = 1000 * 60 * 60 * 24;
 
-    public static String createJwt(String email){
+    private JwtUtil() {}
+
+    public static String generateToken(String email) {
         return Jwts.builder()
                 .subject(email)
                 .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + expirationTime))
-                .signWith(SignatureAlgorithm.HS256, key)
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(privateKey, Jwts.SIG.RS256)
                 .compact();
     }
 
-    public static String extractUsername(String token) {
+    public static String extractEmail(String token) {
         return Jwts.parser()
-                .setSigningKey(key)
+                .verifyWith(publicKey)
                 .build()
-                .parseClaimsJws(token)
-                .getBody()
+                .parseSignedClaims(token)
+                .getPayload()
                 .getSubject();
     }
 
-    public static boolean isExpired(String token){
-        Date expired = Jwts.parser()
-                .setSigningKey(key)
-                .build()
-                .parseClaimsJws(token)
-                .getBody()
-                .getExpiration();
-        return expired.before(new Date());
+    public static boolean validateToken(String token, String email) {
+        try {
+            return extractEmail(token).equals(email);
+        } catch (JwtException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
-    public static boolean validateToken(String token, String username) {
-        String extractedUsername = extractUsername(token);
-        return (extractedUsername.equals(username) && !isExpired(token));
+    private static KeyPair generateKeyPair() {
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            return keyPairGenerator.generateKeyPair();
+        } catch (Exception e) {
+            throw new RuntimeException("Ошибка генерации RSA ключа", e);
+        }
     }
 }
